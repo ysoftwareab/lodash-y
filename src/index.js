076bb9ae4abd0f0699339ebd8702b5a75bafd921
Andrei Neculau (anu) <andrei.neculau@tobii.com>
2018-03-30 22:29:28 +0200

    upgrade eslint-config-firecloud to 0.2.x

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ export let mixins = {
   unbase64
 };
 
+// eslint-disable-next-line firecloud/no-underscore-prefix-exported
 export let _ = (function() {
   let vanillaLodash = __.runInContext();
   vanillaLodash.mixin(mixins);

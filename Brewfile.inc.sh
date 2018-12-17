62c53c5cb740eca91a2674bdfcccce1ecb5781ac
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-17 02:53:43 +0100

    use travis nvm

--- Brewfile.inc.sh
+++ Brewfile.inc.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
 source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-minimal.inc.sh
-source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-node.inc.sh
-
-rm -rf ${NVM_DIR}
-mkdir -p ${NVM_DIR}
-
-nvm install ${NVM_NODE} || true
-nvm use --delete-prefix --silent ${NVM_NODE}
-
-which node
-node --version
-npm config list
+# source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-node.inc.sh
+npm install --global npm

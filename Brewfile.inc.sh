fa727c03627f79b619ce804d86080de7332e546d
Andrei Neculau <andrei.neculau@gmail.com>
2018-12-17 02:31:14 +0100

    test

--- Brewfile.inc.sh
+++ Brewfile.inc.sh
@@ -5,7 +5,9 @@ source ${SUPPORT_FIRECLOUD_DIR}/ci/brew-install-node.inc.sh
 
 rm -rf ${NVM_DIR}
 mkdir -p ${NVM_DIR}
-nvm unalias default
 
 nvm install ${NVM_NODE}
 nvm use --delete-prefix --silent ${NVM_NODE}
+
+which node
+node --version

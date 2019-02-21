fcf42d7d82f3fc72fe289385e04822d5e238bc6b
Andrei Neculau <andrei.neculau@gmail.com>
2019-02-21 20:29:50 +0100

    simplify alwaysPromise

--- src/mixins/always-promise.js
+++ src/mixins/always-promise.js
@@ -2,15 +2,7 @@ export let alwaysPromise = function(maybePromiseLike) {
   // eslint-disable-next-line consistent-this, no-invalid-this
   let _ = this;
 
-  if (!_.isPlainObject(maybePromiseLike) && !_.isFunction(maybePromiseLike)) {
-    return Promise.resolve(maybePromiseLike);
-  }
-
-  if (!_.isFunction(maybePromiseLike.then)) {
-    return Promise.resolve(maybePromiseLike);
-  }
-
-  return maybePromiseLike;
+  return Promise.resolve(maybePromiseLike);
 };
 
 export default alwaysPromise;

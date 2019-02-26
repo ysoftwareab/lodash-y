12010838b44aba92b1a9aaae30a8c29c95d7253c
Andrei Neculau <andrei.neculau@gmail.com>
2019-02-26 22:26:55 +0100

    breaking: remove alwaysPromise mixin. await can be used on non-Promise values

--- src/mixins/always-promise.js
+++
@@ -1,6 +0,0 @@
-export let alwaysPromise = function(maybePromiseLike) {
-  // eslint-disable-next-line consistent-this, babel/no-invalid-this
-  let _ = this;
-
-  return Promise.resolve(maybePromiseLike);
-};

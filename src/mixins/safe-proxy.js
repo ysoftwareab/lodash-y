e1e768138d2841506ece75862712a28e930751df
Andrei Neculau <andrei.neculau@gmail.com>
2019-03-14 20:53:15 +0100

    breaking: remove overloaded _ property from safeProxy; prefer 'in' check instead

--- src/mixins/safe-proxy.js
+++ src/mixins/safe-proxy.js
@@ -1,3 +1,7 @@
+// NOTE if you need to check if an env var exist, use
+// '<var>' in someProxy
+// e.g. if ('PATH' in _.safeProxy(process.env)) { ...do smth with _.safeProxy(process.env).PATH... }
+
 export let safeProxy = function(env) {
   // eslint-disable-next-line consistent-this, no-invalid-this
   let _ = this;
@@ -5,9 +9,6 @@ export let safeProxy = function(env) {
   // eslint-disable-next-line fp/no-proxy
   return new Proxy(env, {
     get: function(target, property, _receiver) {
-      if (property === '_') {
-        return target;
-      }
       if (!_.isString(target[property])) {
         throw new Error(`${property} is undefined.`);
       }

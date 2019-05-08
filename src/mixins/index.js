a62de48dfa114035e700474d6cd82290c667b1f4
Andrei Neculau <andrei.neculau@gmail.com>
2019-05-08 10:29:56 +0200

    0.2.15 (dist)

---
+++ src/mixins/index.js
@@ -0,0 +1,78 @@
+
+import {
+  abstract
+} from './abstract.js';
+
+import {
+  alwaysPromise
+} from './always-promise.js';
+
+import {
+  base64
+} from './base64.js';
+
+import {
+  callbackify
+} from './callbackify.js';
+
+import {
+  consoleLogTime
+} from './console-log-time.js';
+
+import {
+  defer
+} from './defer.js';
+
+import {
+  hardRejection
+} from './hard-rejection.js';
+
+import {
+  mapValuesDeep
+} from './map-values-deep.js';
+
+import {
+  naiveChecksum
+} from './naive-checksum.js';
+
+import {
+  onceIn
+} from './once-in.js';
+
+import {
+  outdent
+} from './outdent.js';
+
+import {
+  promisify
+} from './promisify.js';
+
+import {
+  safeProxy
+} from './safe-proxy.js';
+
+import {
+  sleep
+} from './sleep.js';
+
+import {
+  unbase64
+} from './unbase64.js';
+
+export default {
+  abstract,
+  alwaysPromise,
+  base64,
+  callbackify,
+  consoleLogTime,
+  defer,
+  hardRejection,
+  mapValuesDeep,
+  naiveChecksum,
+  onceIn,
+  outdent,
+  promisify,
+  safeProxy,
+  sleep,
+  unbase64
+};

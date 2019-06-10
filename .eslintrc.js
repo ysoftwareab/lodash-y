58fedc8c23f60584674913ef5beb51dcebf9f485
Andrei Neculau <andrei.neculau@gmail.com>
2019-06-10 09:13:19 +0200

    use makeTsConfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,67 +1,31 @@
+// temporary workaround because overrides doesn't allow extends (yet!)
+// see https://github.com/eslint/eslint/issues/8813#issuecomment-456034732
+// see https://github.com/eslint/eslint/pull/11554
+let {
+  makeTsConfig
+} = require('eslint-config-firecloud/util');
+
 module.exports = {
   root: true,
 
   extends: [
     'firecloud/node'
+  ],
+
+  overrides: [
+    makeTsConfig({
+      files: [
+        '*.ts'
+      ],
+
+      extends: [
+        'firecloud/configs/typescript'
+      ],
+
+      rules: {
+        // FIXME gives Allocation failed - JavaScript heap out of memory
+        'firecloud/order-imports': 'off'
+      }
+    })
   ]
 };
-
-let tsConfig = {
-  files: [
-    '*.ts'
-  ],
-
-  extends: [
-    'firecloud/configs/typescript'
-  ],
-
-  rules: {
-    // FIXME gives Allocation failed - JavaScript heap out of memory
-    'firecloud/order-imports': 'off'
-  }
-};
-
-// temporary workaround because overrides doesn't allow extends (yet!)
-// see https://github.com/eslint/eslint/issues/8813#issuecomment-456034732
-
-// let _ = require('lodash-firecloud');
-let _ = require('.');
-
-let _defaultsValues = function(...objs) {
-  let keys = _.reduce(objs, function(acc, obj, _index) {
-    acc = _.concat(acc, _.keys(obj));
-    acc = _.uniq(acc);
-    return acc;
-  }, []);
-
-  let result = _.reduce(keys, function(acc, key, _index) {
-    let objsKey = _.map(objs, key);
-
-    if (_.some(objsKey, _.isPlainObject)) {
-      acc[key] = _.defaults({}, ...objsKey);
-    } else {
-      acc[key] = _.find(objsKey, function(value) {
-        return !_.isUndefined(value);
-      });
-    }
-
-    return acc;
-  }, {});
-
-  return result;
-};
-
-delete tsConfig.extends;
-let ecfTypescript = require('eslint-config-firecloud/configs/typescript');
-
-delete ecfTypescript.extends;
-let tsEslintRecommended = require('@typescript-eslint/eslint-plugin/dist/configs/recommended.json');
-
-delete tsEslintRecommended.extends;
-let tsEslintBase = require('@typescript-eslint/eslint-plugin/dist/configs/base.json');
-
-tsConfig = _defaultsValues(tsConfig, ecfTypescript, tsEslintRecommended, tsEslintBase);
-
-module.exports.overrides = [
-  tsConfig
-];

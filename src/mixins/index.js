de098fd1f21ec2edd7e52398950fd3ae1fc60b51
Andrei Neculau <andrei.neculau@gmail.com>
2019-08-08 00:28:14 +0200

    0.4.1 (dist)

---
+++ src/mixins/index.js
@@ -0,0 +1,95 @@
+
+import {
+  abstract
+} from './abstract';
+
+import {
+  base64
+} from './base64';
+
+import {
+  callbackify
+} from './callbackify';
+
+import {
+  consoleLogTime
+} from './console-log-time';
+
+import {
+  defer
+} from './defer';
+
+import {
+  getStackTrace
+} from './get-stack-trace';
+
+import {
+  getV8OpenHandles
+} from './get-v8-open-handles';
+
+import {
+  hardRejection
+} from './hard-rejection';
+
+import {
+  isDefined
+} from './is-defined';
+
+import {
+  mapValuesDeep
+} from './map-values-deep';
+
+import {
+  naiveChecksum
+} from './naive-checksum';
+
+import {
+  onceIn
+} from './once-in';
+
+import {
+  outdent
+} from './outdent';
+
+import {
+  promisify
+} from './promisify';
+
+import {
+  safeProxy
+} from './safe-proxy';
+
+import {
+  sleep
+} from './sleep';
+
+import {
+  throttleExp
+} from './throttle-exp';
+
+import {
+  unbase64
+} from './unbase64';
+
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  abstract,
+  base64,
+  callbackify,
+  consoleLogTime,
+  defer,
+  getStackTrace,
+  getV8OpenHandles,
+  hardRejection,
+  isDefined,
+  mapValuesDeep,
+  naiveChecksum,
+  onceIn,
+  outdent,
+  promisify,
+  safeProxy,
+  sleep,
+  throttleExp,
+  unbase64
+};

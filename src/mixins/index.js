131a4498fd52a674ef4773a46a2b9c33158b04fe
Andrei Neculau <andrei.neculau@gmail.com>
2019-10-05 21:29:15 +0200

    0.4.10 (dist)

---
+++ src/mixins/index.js
@@ -0,0 +1,115 @@
+
+import {
+  abstract
+} from './abstract';
+
+import {
+  base64
+} from './base64';
+
+import {
+  callbackify
+} from './callbackify';
+
+import {
+  consoleLogTime
+} from './console-log-time';
+
+import {
+  defer
+} from './defer';
+
+import {
+  getStackTrace
+} from './get-stack-trace';
+
+import {
+  getV8OpenHandles
+} from './get-v8-open-handles';
+
+import {
+  hardRejection
+} from './hard-rejection';
+
+import {
+  isDefined
+} from './is-defined';
+
+import {
+  limitInParallel
+} from './limit-in-parallel';
+
+import {
+  mapValuesDeep
+} from './map-values-deep';
+
+import {
+  memoizeTtl
+} from './memoize-ttl';
+
+import {
+  naiveChecksum
+} from './naive-checksum';
+
+import {
+  onceIn
+} from './once-in';
+
+import {
+  outdent
+} from './outdent';
+
+import {
+  promisify
+} from './promisify';
+
+import {
+  requireDir
+} from './require-dir';
+
+import {
+  safeProxy
+} from './safe-proxy';
+
+import {
+  sleep
+} from './sleep';
+
+import {
+  throttleExp
+} from './throttle-exp';
+
+import {
+  throttleTrue
+} from './throttle-true';
+
+import {
+  unbase64
+} from './unbase64';
+
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  abstract,
+  base64,
+  callbackify,
+  consoleLogTime,
+  defer,
+  getStackTrace,
+  getV8OpenHandles,
+  hardRejection,
+  isDefined,
+  limitInParallel,
+  mapValuesDeep,
+  memoizeTtl,
+  naiveChecksum,
+  onceIn,
+  outdent,
+  promisify,
+  requireDir,
+  safeProxy,
+  sleep,
+  throttleExp,
+  throttleTrue,
+  unbase64
+};

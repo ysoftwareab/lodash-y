53dfec95e7bc9d28536b091aec384aad9e480d8e
Andrei Neculau <andrei.neculau@gmail.com>
2019-10-19 13:14:53 +0200

    0.4.11 (dist)

---
+++ src/mixins/index.js
@@ -0,0 +1,130 @@
+
+import {
+  abstract
+} from './abstract';
+
+import {
+  base64
+} from './base64';
+
+import {
+  callbackify
+} from './callbackify';
+
+import {
+  consoleLogTime
+} from './console-log-time';
+
+import {
+  defer
+} from './defer';
+
+import {
+  deferred
+} from './deferred';
+
+import {
+  getStackTrace
+} from './get-stack-trace';
+
+import {
+  getV8OpenHandles
+} from './get-v8-open-handles';
+
+import {
+  hardRejection
+} from './hard-rejection';
+
+import {
+  isDefined
+} from './is-defined';
+
+import {
+  limitInParallel
+} from './limit-in-parallel';
+
+import {
+  mapValuesDeep
+} from './map-values-deep';
+
+import {
+  memoizeTtl
+} from './memoize-ttl';
+
+import {
+  mergeConcatArrays
+} from './merge-concat-arrays';
+
+import {
+  mergeWithoutArrays
+} from './merge-without-arrays';
+
+import {
+  naiveChecksum
+} from './naive-checksum';
+
+import {
+  onceIn
+} from './once-in';
+
+import {
+  outdent
+} from './outdent';
+
+import {
+  promisify
+} from './promisify';
+
+import {
+  requireDir
+} from './require-dir';
+
+import {
+  safeProxy
+} from './safe-proxy';
+
+import {
+  sleep
+} from './sleep';
+
+import {
+  throttleExp
+} from './throttle-exp';
+
+import {
+  throttleTrue
+} from './throttle-true';
+
+import {
+  unbase64
+} from './unbase64';
+
+
+// eslint-disable-next-line import/no-default-export
+export default {
+  abstract,
+  base64,
+  callbackify,
+  consoleLogTime,
+  defer,
+  deferred,
+  getStackTrace,
+  getV8OpenHandles,
+  hardRejection,
+  isDefined,
+  limitInParallel,
+  mapValuesDeep,
+  memoizeTtl,
+  mergeConcatArrays,
+  mergeWithoutArrays,
+  naiveChecksum,
+  onceIn,
+  outdent,
+  promisify,
+  requireDir,
+  safeProxy,
+  sleep,
+  throttleExp,
+  throttleTrue,
+  unbase64
+};

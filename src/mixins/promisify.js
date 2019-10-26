e36be2fe670e8c9d82fb677c000dab14505cd4eb
Andrei Neculau <andrei.neculau@gmail.com>
2019-10-26 22:32:49 +0200

    use deferred in promisify

--- src/mixins/promisify.js
+++ src/mixins/promisify.js
@@ -4,39 +4,44 @@ export let promisify = function(origFn, {
   callbackFirst = false,
   errorInCallback = true
 } = {}) {
-  let fn = function(...args) {
-    return new Promise(function(resolve, reject) {
-      let callback = function(...results) {
-        if (errorInCallback) {
-          let err = results.shift();
-          if (err) {
-            reject(err);
-            return;
-          }
+  // eslint-disable-next-line consistent-this, babel/no-invalid-this
+  let _ = this;
+
+  let fn = async function(...args) {
+    let deferred = _.deferred();
+
+    let callback = function(...results) {
+      if (errorInCallback) {
+        let err = results.shift();
+        if (err) {
+          deferred.reject(err);
+          return;
         }
-
-        switch (results.length) {
-        case 0:
-          resolve();
-          break;
-
-        case 1:
-          resolve(results[0]);
-          break;
-
-        default:
-          resolve(results);
-          break;
-        }
-      };
-
-      if (callbackFirst) {
-        args.unshift(callback);
-      } else {
-        args.push(callback);
       }
-      origFn(...args);
-    });
+
+      switch (results.length) {
+      case 0:
+        deferred.resolve();
+        break;
+
+      case 1:
+        deferred.resolve(results[0]);
+        break;
+
+      default:
+        deferred.resolve(results);
+        break;
+      }
+    };
+
+    if (callbackFirst) {
+      args.unshift(callback);
+    } else {
+      args.push(callback);
+    }
+    origFn(...args);
+
+    return deferred.promise;
   };
 
   Object.setPrototypeOf(fn, Object.getPrototypeOf(origFn));

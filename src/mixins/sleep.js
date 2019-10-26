f61a076e2b221c805b0480a64f5cde03fe212d15
Andrei Neculau <andrei.neculau@gmail.com>
2019-10-26 22:34:12 +0200

    use deferred in sleep

--- src/mixins/sleep.js
+++ src/mixins/sleep.js
@@ -1,8 +1,8 @@
-export let sleep = function(ms = 0) {
+export let sleep = async function(ms = 0) {
   // eslint-disable-next-line consistent-this, babel/no-invalid-this
   let _ = this;
 
-  return new Promise(function(resolve, _reject) {
-    _.delay(resolve, ms);
-  });
+  let deferred = _.deferred();
+  _.delay(deferred.resolve, ms);
+  await deferred.promise;
 };

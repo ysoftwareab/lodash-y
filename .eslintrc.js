5fc5d1df4f2c4e41ab5a9ce370735f8be656ce05
Andrei Neculau <andrei.neculau@gmail.com>
2019-10-27 00:02:24 +0200

    custom valid-jsdoc rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,3 @@
-// temporary workaround because overrides doesn't allow extends (yet!)
-// see https://github.com/eslint/eslint/issues/8813#issuecomment-456034732
-// see https://github.com/eslint/eslint/pull/11554
-let {
-  makeTsConfig
-} = require('eslint-config-firecloud/util');
-
 module.exports = {
   root: true,
 
@@ -12,20 +5,44 @@ module.exports = {
     'firecloud/node'
   ],
 
-  overrides: [
-    makeTsConfig({
-      files: [
-        '*.ts'
-      ],
-
-      extends: [
-        'firecloud/configs/typescript'
-      ],
-
-      rules: {
-        // FIXME gives Allocation failed - JavaScript heap out of memory
-        'firecloud/order-imports': 'off'
-      }
-    })
-  ]
+  rules: {
+    // https://github.com/microsoft/TypeScript-wiki/blob/master/JSDoc-support-in-JavaScript.md
+    'valid-jsdoc': ['error', {
+      prefer: {
+        // no synonyms
+        arg: 'param',
+        argument: 'param',
+        const: 'constant',
+        defaultvalue: 'default',
+        desc: 'description',
+        emits: 'fires',
+        exception: 'throws',
+        extends: 'augments',
+        fileoverview: 'file',
+        func: 'function',
+        host: 'external',
+        method: 'function',
+        overview: 'file',
+        return: 'returns',
+        var: 'member',
+        virtual: 'abstract',
+        yield: 'yields'
+      },
+      preferType: {
+        BigInt: 'bigint',
+        Boolean: 'boolean',
+        Null: 'null',
+        Number: 'number',
+        String: 'string',
+        Symbol: 'symbol',
+        Undefined: 'undefined',
+        Void: 'void',
+        array: 'Array',
+        function: 'Function',
+        object: 'Object'
+      },
+      requireParamDescription: false,
+      requireReturnDescription: false
+    }]
+  }
 };

b498d5f0034f0186c72a74a853981891e19f6425
Andrei Neculau <andrei.neculau@gmail.com>
2019-10-26 22:32:20 +0200

    onceIn style

--- src/mixins/once-in.js
+++ src/mixins/once-in.js
@@ -1,6 +1,5 @@
-export let onceIn = function(...args) {
-  // eslint-disable-next-line consistent-this, babel/no-invalid-this
-  let _ = this;
+import {
+  throttleTrue
+} from './throttle-true';
 
-  return _.throttleTrue(...args);
-};
+export let onceIn = throttleTrue;

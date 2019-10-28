a94445432dce7c0081650477b1bb816f3c897475
Andrei Neculau <andrei.neculau@gmail.com>
2019-10-28 11:08:04 +0100

    remove valid-jsdoc rule. now in eslint-config-firecloud

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,46 +3,5 @@ module.exports = {
 
   extends: [
     'firecloud/node'
-  ],
-
-  rules: {
-    // https://github.com/microsoft/TypeScript-wiki/blob/master/JSDoc-support-in-JavaScript.md
-    'valid-jsdoc': ['error', {
-      prefer: {
-        // no synonyms
-        arg: 'param',
-        argument: 'param',
-        const: 'constant',
-        defaultvalue: 'default',
-        desc: 'description',
-        emits: 'fires',
-        exception: 'throws',
-        extends: 'augments',
-        fileoverview: 'file',
-        func: 'function',
-        host: 'external',
-        method: 'function',
-        overview: 'file',
-        return: 'returns',
-        var: 'member',
-        virtual: 'abstract',
-        yield: 'yields'
-      },
-      preferType: {
-        BigInt: 'bigint',
-        Boolean: 'boolean',
-        Null: 'null',
-        Number: 'number',
-        String: 'string',
-        Symbol: 'symbol',
-        Undefined: 'undefined',
-        Void: 'void',
-        array: 'Array',
-        function: 'Function',
-        object: 'Object'
-      },
-      requireParamDescription: false,
-      requireReturnDescription: false
-    }]
-  }
+  ]
 };

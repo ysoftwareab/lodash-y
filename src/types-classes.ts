7fb1068fc12602f01dc5e33ad25c5ee4c9f2a96b
Andrei Neculau <andrei.neculau@gmail.com>
2019-11-20 11:08:53 +0100

    add Class-related types

---
+++ src/types-classes.ts
@@ -0,0 +1,45 @@
+/**
+ * A class type (i.e. A constructor type).
+ */
+export type Class = {
+  new(...args: any[]): any;
+};
+
+/**
+ * Construct a class type with the static properties of TClass except for those in type TKey.
+ */
+export type OmitClassStatic<
+  TClass extends Class,
+  TKey extends keyof any
+> =
+  TClass extends {
+    new(...args: infer TArgs): infer TInstance;
+  }
+    ? (
+      & {
+        new(...args: TArgs): TInstance;
+      }
+      & Omit<TClass, TKey>
+    )
+    : never;
+
+/**
+ * Construct a class type with the instance properties of TClass except for those in type TKey.
+ */
+export type OmitClassInstance<
+  TClass extends Class,
+  TKey extends keyof any
+> =
+  TClass extends {
+    new(...args: infer TArgs): infer TInstance;
+  }
+    ? (
+      & {
+        new(...args: TArgs): Omit<TInstance, TKey>;
+      }
+      & Omit<TClass, 'prototype'>
+      & {
+        prototype: Omit<TInstance, TKey>;
+      }
+    )
+    : never;
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
 export * from './types-callbacks';
 
+export * from './types-classes';
+
 export * from './types-core';
 
 export * from './types-functions';

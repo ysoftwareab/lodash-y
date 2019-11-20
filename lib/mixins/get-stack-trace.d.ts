97cfbb9da1ccb095df773df439aef8e542ce2c24
Andrei Neculau <andrei.neculau@gmail.com>
2019-11-20 11:34:01 +0100

    0.5.6 (dist)

---
+++ lib/mixins/get-stack-trace.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="node" />
+/**
+ * Part of `lodash-firecloud`.
+ *
+ * Gets the current stacktrace.
+ *
+ * @param level The maximum stacktrace length.
+ * @returns Returns a structured stacktrace, that is a list of CallSite objects.
+ */
+export declare let getStackTrace: (level?: number) => NodeJS.CallSite[];
---
+++ lib/mixins/outdent.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Part of `lodash-firecloud`.
+ *
+ * Tag to outdent template literals.
+ *
+ * @param strings Template strings.
+ * @param values Values.
+ * @returns Returns an outdented string.
+ */
+export declare let outdent: import("outdent").Outdent;

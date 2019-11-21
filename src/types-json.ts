d01b693574bbad8ddd39e75db5a3039a77a3bdf9
Andrei Neculau <andrei.neculau@gmail.com>
2019-11-21 12:37:57 +0100

    add reference to new functionality in ts 3.7

--- src/types-json.ts
+++ src/types-json.ts
@@ -1,3 +1,6 @@
+// only in TypeScript 3.7+
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-7.html#more-recursive-type-aliases
+// export type JsonArray = JsonValue[];
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface JsonArray extends Array<JsonValue> {}
 

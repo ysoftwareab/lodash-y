f7a8684cd4c7188533fc376304fcd5388da307e5
Andrei Neculau <andrei.neculau@gmail.com>
2019-11-21 15:43:21 +0100

    0.5.9 (dist)

---
+++ lib/mixins/deferred.d.ts
@@ -0,0 +1,35 @@
+import { Fn } from '../types';
+export interface Deferred<TValue> {
+    /**
+     * The internal promise.
+     */
+    promise: Promise<TValue>;
+    /**
+     * The state of the promise.
+     */
+    state: 'pending' | 'resolved' | 'rejected';
+    /**
+     * The resolve callback of the promise.
+     */
+    resolve: Fn<void, [TValue?]>;
+    /**
+     * The reject callback of the promise.
+     */
+    reject: Fn<void, [Error]>;
+    /**
+     * The value that the promise resolved with.
+     */
+    value?: TValue;
+    /**
+     * The error that the promise rejected with.
+     */
+    err?: Error;
+}
+/**
+ * Part of `lodash-firecloud`.
+ *
+ * Create a Deferred object that references the promise, the resolve and reject functions.
+ *
+ * @returns Returns the Deferred object.
+ */
+export declare let deferred: <TValue = unknown>() => Deferred<TValue>;
---
+++ lib/types.d.ts
@@ -0,0 +1,5 @@
+export * from './types-callbacks';
+export * from './types-classes';
+export * from './types-core';
+export * from './types-functions';
+export * from './types-json';

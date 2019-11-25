a0c534190f5018df9a78cf697fed25c8be72e796
Andrei Neculau <andrei.neculau@gmail.com>
2019-11-25 11:41:21 +0100

    add utility-types

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,10 @@
+import * as _UT from 'utility-types';
+
+// https://github.com/piotrwitek/utility-types is a good library
+// but some types are not properly tested
+// e.g. Primitive didn't have undefined and null until 2019-10-21, since added in 2019-04-02
+export let UT = _UT;
+
 export * from './types-callbacks';
 
 export * from './types-classes';

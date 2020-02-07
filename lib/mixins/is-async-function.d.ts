70eb0fbca14932712a3c36f6f95d69c58c9e34ed
Andrei Neculau <andrei.neculau@gmail.com>
2020-02-07 01:18:49 +0100

    0.5.22 (dist)

---
+++ lib/mixins/is-async-function.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Part of `lodash-firecloud`.
+ *
+ * Checks if value is classified as an async Function object.
+ *
+ * @async
+ * @param [value] The value to check.
+ * @returns Returns true if value is an async function, else false.
+ */
+export declare let isAsyncFunction: (value: unknown) => boolean;

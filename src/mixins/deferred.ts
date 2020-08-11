af23c5da8e63045d554abe3479d564a825957a29
Andrei Neculau <andrei.neculau@gmail.com>
2020-08-11 15:14:00 +0200

    add example for _.deferred. see #30

--- src/mixins/deferred.ts
+++ src/mixins/deferred.ts
@@ -15,6 +15,15 @@ export {
  *
  * Create a Deferred object that references the promise, the resolve and reject functions.
  *
+ * Example usage:
+ * let d = _.deferred();
+ * emitter.on('result', function(result) {d.resolve(result);});
+ * emitter.on('error', function(err) {d.reject(err);});
+ * return d.promise;.
+ *
+ * _.deferred can also wrap a Promise,
+ * and make it more convenient to read Promise state and resolved/rejected values.
+ *
  * @param [promise] A promise to automatically resolve/reject the Deferred object with.
  * @returns Returns the Deferred object.
  */

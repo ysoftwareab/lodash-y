06e381bd78e1d5f185c1e39d3bf7ced608d568b0
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-14 11:32:04 +0100

    use nvm for switch node versions in CI

--- Brewfile.inc.sh
+++ Brewfile.inc.sh
@@ -2,3 +2,6 @@
 
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-minimal.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-node.inc.sh
+source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-node.nvm.inc.sh
+
+nvm use ${SF_NODE_VSN:-latest}

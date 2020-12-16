ac81f667780c66c745a19ce8ca4ade6c8661d45a
Andrei Neculau <andrei.neculau@gmail.com>
2020-12-16 14:15:01 +0100

    fixup! switch to github actions from travis ci

--- Brewfile.inc.sh
+++ Brewfile.inc.sh
@@ -4,4 +4,6 @@ source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-minimal.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-node.inc.sh
 source ${SUPPORT_FIRECLOUD_DIR}/bootstrap/brew-install-node.nvm.inc.sh
 
-nvm use ${SF_NODE_VSN:-latest}
+SF_NODE_VSN=${SF_NODE_VSN:-node}
+nvm install ${SF_NODE_VSN}
+nvm use ${SF_NODE_VSN}
